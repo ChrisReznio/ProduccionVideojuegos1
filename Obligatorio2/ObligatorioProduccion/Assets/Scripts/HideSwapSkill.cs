de2b3d38321103e96e0bdafa007136f5277aee5f
ChrisReznio <chrisreznio@gmail.com>
2020-11-11 18:29:43 -0300

    Now added the key assigned to each skill in the skill bar plus 2 more skills were added

---
+++ Obligatorio2/ObligatorioProduccion/Assets/Scripts/HideSwapSkill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideSwapSkill : MonoBehaviour
+{
+    void Update()
+    {
+        var child = transform.GetChild(0).gameObject;
+        if (GameObject.FindGameObjectWithTag("Body").GetComponent<BodyController>().canBeDamaged)
+        {
+            child.SetActive(true);
+        }
+        else
+        {
+            child.SetActive(false);
+        }
+    }
+}
