b2277bc18175003853fa6ae66e43ec80104555e6
Evge Sidagis <evgenysidagis@gmail.com>
2020-11-16 11:48:30 -0300

    added a few more options for the ingame pause menu. Controls, rety, volume

--- Obligatorio2/ObligatorioProduccion/Assets/Scripts/PauseMenu.cs
+++ Obligatorio2/ObligatorioProduccion/Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
     public static bool isPaused;
+    public AudioMixer audioMixer;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -40,5 +43,12 @@ public class PauseMenu : MonoBehaviour
     {
         isPaused = false;
         SceneManager.LoadScene(0);
+    }
+
+    
+    public void SetVolume(float volume)
+    {
+        Debug.Log("volume");
+        audioMixer.SetFloat("volume", volume);
     }
 }
