1fce765365b49c4caa80922bc44450d2b86c7511
MauriMiguez <mauri.miguez@hotmail.com>
2020-11-17 21:33:50 -0300

    feat: map transition and final message

---
+++ Obligatorio2/ObligatorioProduccion/Assets/Scripts/LoadNewMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewMap : MonoBehaviour
+{
+    public string levelToLoad;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Body")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
