3cdc94d02eb78bebf0ca7ededac9da0aeab35e05
jades98 <jades98@hotmail.es>
2020-12-02 15:56:37 -0300

    Se implementan las particulas en el line renderer

--- Obligatorio2/ObligatorioProduccion/Assets/Scripts/LineController.cs
+++ Obligatorio2/ObligatorioProduccion/Assets/Scripts/LineController.cs
@@ -10,6 +10,16 @@ public class LineController : MonoBehaviour
     Gradient gradient;
     GradientColorKey[] colorKey;
     GradientAlphaKey[] alphaKey;
+    private float posBodyX;
+    private float posBodyY;
+    private float posSoulX;
+    private float posSoulY;
+    public GameObject lifelineParticles;
+    private GameObject localParticlesMiddle;
+    private GameObject localParticlesBody;
+    private GameObject localParticlesSoul;
+    private GameObject localParticlesCloserBody;
+    private GameObject localParticlesCloserSoul;
 
 
     void Start()
@@ -23,7 +33,7 @@ public class LineController : MonoBehaviour
         line.endWidth = 0.0f;
         line.material = new Material(Shader.Find("Sprites/Default"));
 
-        line.sortingLayerName = "WorldObject";
+        line.sortingLayerName = "Player";
 
         line.startColor = line.endColor = new Color(1, 0, 0, 1);
      
@@ -44,9 +54,15 @@ public class LineController : MonoBehaviour
         alphaKey[1].time = 1.0f;
 
         gradient.SetKeys(colorKey, alphaKey);
-
      
         line.colorGradient = gradient;
+
+        Vector3 posInit = new Vector3(0, 0, 0);
+        localParticlesMiddle = Instantiate(lifelineParticles, posInit, Quaternion.Euler(Vector3.zero));
+        localParticlesBody = Instantiate(lifelineParticles, posInit, Quaternion.Euler(Vector3.zero));
+        localParticlesSoul = Instantiate(lifelineParticles, posInit, Quaternion.Euler(Vector3.zero));
+        localParticlesCloserBody = Instantiate(lifelineParticles, posInit, Quaternion.Euler(Vector3.zero));
+        localParticlesCloserSoul = Instantiate(lifelineParticles, posInit, Quaternion.Euler(Vector3.zero));
     }
     
 
@@ -62,6 +78,34 @@ public class LineController : MonoBehaviour
             
             line.SetPosition(0, body.transform.localPosition);
             line.SetPosition(1, soul.transform.localPosition);
+
+            posBodyX = body.transform.position.x;
+            posBodyY = body.transform.position.y;
+            posSoulX = soul.transform.position.x;
+            posSoulY = soul.transform.position.y;
+            Vector3 posCloserBody = new Vector3((posBodyX + 1 * (posSoulX - posBodyX) / 6), (posBodyY + 1* (posSoulY - posBodyY) / 6), 0);
+            Vector3 posCloseBody = new Vector3((posBodyX + 2*(posSoulX - posBodyX) / 6), (posBodyY + 2 * (posSoulY - posBodyY) / 6), 0);
+            Vector3 posMiddle = new Vector3((posBodyX + (posSoulX - posBodyX) / 2), (posBodyY + (posSoulY - posBodyY) / 2), 0);
+            Vector3 posCloseSoul = new Vector3((posBodyX + 4*(posSoulX - posBodyX) / 6), (posBodyY + 4 * (posSoulY - posBodyY) / 6), 0);
+            Vector3 posCloserSoul = new Vector3((posBodyX + 5 * (posSoulX - posBodyX) / 6), (posBodyY + 5 * (posSoulY - posBodyY) / 6), 0);
+            localParticlesMiddle.transform.position = posMiddle;
+            localParticlesBody.transform.position = posCloseBody;
+            localParticlesSoul.transform.position = posCloseSoul;
+            localParticlesCloserBody.transform.position = posCloserBody;
+            localParticlesCloserSoul.transform.position = posCloserSoul;
+            localParticlesMiddle.SetActive(true);
+            localParticlesBody.SetActive(true);
+            localParticlesSoul.SetActive(true);
+            localParticlesCloserBody.SetActive(true);
+            localParticlesCloserSoul.SetActive(true);
+        }
+        else
+        {
+            localParticlesMiddle.SetActive(false);
+            localParticlesBody.SetActive(false);
+            localParticlesSoul.SetActive(false);
+            localParticlesCloserBody.SetActive(false);
+            localParticlesCloserSoul.SetActive(false);
         }
 
     }
