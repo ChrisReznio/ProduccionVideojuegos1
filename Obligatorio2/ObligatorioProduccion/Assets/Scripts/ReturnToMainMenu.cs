be8d69b34088b7ca3511333729f355e6032bfb6d
jades98 <jades98@hotmail.es>
2020-12-21 16:53:04 -0300

    Se hace que la pantalla game won solo se acabe al apretar espacio.

--- Obligatorio2/ObligatorioProduccion/Assets/Scripts/ReturnToMainMenu.cs
+++ Obligatorio2/ObligatorioProduccion/Assets/Scripts/ReturnToMainMenu.cs
@@ -15,7 +15,7 @@ public class ReturnToMainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(MainMenu);
         }
